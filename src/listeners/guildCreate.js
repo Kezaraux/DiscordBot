cc1a998a558080a46e937e88d46fb39be78b75fe
Brendan Doyle <bdoyle@ourmagnet.net>
2019-10-16 13:26:57 -0400

    Added message when bot joins server

--- src/listeners/guildCreate.js
+++ src/listeners/guildCreate.js
@@ -2,6 +2,8 @@ import { Listener } from "discord-akairo";
 
 import log, { logObj } from "../utils/logger";
 import { updateGuildConfig } from "../actions";
+import { getGuildConfig } from "../selectors";
+import { getDefaultChannel } from "../utils/discord-helpers";
 const initialConfig = require("../../config/initialDbConfig.json");
 
 const event = "guildCreate";
@@ -22,6 +24,13 @@ class GuildCreateListener extends Listener {
       updateGuildConfig({ guild_id: guild.id, config: initialConfig })
     );
     log(`Config created for guild ${guild.name}`);
+    const config = getGuildConfig(this.client.store.getState(), guild.id);
+    const defaultChannel = getDefaultChannel(guild);
+    defaultChannel.send(
+      "Hi! Thanks for adding me to your server!\n" +
+        `The default prefix for my commands is: \`${config.prefix}\`` +
+        `\nTry it out with \`${config.prefix}help\` to see a list of my commands!`
+    );
   }
 }
 
---
+++ src/utils/discord-helpers.js
@@ -0,0 +1,32 @@
+import long from "long";
+
+export const getDefaultChannel = guild => {
+  // Original default channel
+  if (guild.channels.has(guild.id)) {
+    return guild.channels.get(guild.id);
+  }
+
+  // General channel
+  const generalChannel = guild.channels.find(c => c.name === "general");
+  if (generalChannel) {
+    return generalChannel;
+  }
+
+  // Find the highest text channel the bot can talk in
+  return guild.channels.filter(
+    c =>
+      c.type === "text" &&
+      c
+        .permissionsFor(guild.client.user)
+        .has("SEND_MESSAGES")
+        .sort(
+          (a, b) =>
+            a.position - b.position ||
+            long
+              .fromString(a.id)
+              .sub(long.fromString(b.id))
+              .toNumber()
+        )
+        .first()
+  );
+};
