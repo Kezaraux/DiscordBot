fe45fd29181c896d452058a0172999057813a67c
Brendan <brendan.doyl3@gmail.com>
2019-10-20 22:32:18 -0400

    Start of play command

---
+++ src/commands/play.js
@@ -0,0 +1,47 @@
+import { Command } from "discord-akairo";
+import config from "config";
+import { sprintf } from "sprintf-js";
+
+import log from "../utils/logger";
+import { constructSong, queueSong } from "../utils/music-helpers";
+import { joinVoiceChannel } from "../utils/discord-helpers"
+
+const command = "play";
+const aliases = [command, "p"];
+const category = "music";
+
+class PlayCommand extends Command {
+    constructor() {
+        super(command, {
+            aliases,
+            category,
+            args: [{ id: "song", type: "string" }]
+        });
+        log(`${command}Command created`);
+    }
+
+    async exec(message, args) {
+        if (args.song) {  // Queue the song                 
+            const builtSong = await constructSong(args.song);
+            if (builtSong) {
+                message.channel.send(queueSong(builtSong, this.client.store))
+            } else {
+                return message.channel.send(sprintf(ResourceStrings.error_item_not_valid, "URL"));
+            }
+        }
+
+        // Start playing the queue
+        return message.channel.send(await joinVoiceChannel(message, this.client.store, this.client.user));
+    }
+}
+
+export const help = {
+    isEnabled: config.get(`features.${category}`) || true,
+    identifier: command,
+    usage: `${command}`,
+    aliases,
+    category,
+    blurb: "A basic command to see if the bot works!" + "\nThe bot will respond with 'Pong!'"
+};
+
+export default PlayCommand;
--- src/utils/discord-helpers.js
+++ src/utils/discord-helpers.js
@@ -1,6 +1,8 @@
 import long from "long";
 
 import { permissions } from "./constants";
+import { getVoiceConnection } from "../selectors";
+import ResourceStrings from "./ResourceStrings.json";
 
 export const getDefaultChannel = guild => {
     // Original default channel
@@ -36,3 +38,32 @@ export const getDefaultChannel = guild => {
 export const findChannel = (guild, channelId) => guild.channels.find(c => c.id === channelId);
 
 export const hasPermissionFor = (channel, roleOrMem, perm) => channel.permissionsFor(roleOrMem).has(perm);
+
+export const isUserInVoiceChannel = msg => {
+    return Boolean(msg.member.voiceChannel);
+}
+
+export const isBotInVoiceChannel = store => {
+    return Boolean(getVoiceConnection(store.getState()));
+}
+
+export const joinVoiceChannel = async (msg, store, user) => {
+    if (getVoiceConnection(store.getState())) {
+        return ResourceStrings.error_already_in_voice
+    }
+
+    const vc = msg.member.voiceChannel;
+    if (!vc) {
+        return ResourceStrings.error_not_in_voice;
+    }
+
+    const botGuildMem = await msg.guild.fetchMember(user);
+    const thing = msg.client.user;
+    const perms = vc.permissionsFor(thing);
+    if (!perms.has(permissions.CONNECT) || !perms.has(permissions.SPEAK)) {
+        console.log("THIS IS HAPPENING");
+        return ResourceStrings.error_perms_to_join_and_speak;
+    }
+
+    return "Boop";
+}
